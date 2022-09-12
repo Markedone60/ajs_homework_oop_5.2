d3a08af4dd6e6e798a60ade2b71902427ddec04c
Grigory <gregory.rvl@yandex.ru>
2022-09-12 18:10:58 +0300

    second commit - fixing lint errors

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,5 @@
 import Character from './character';
+
+const warrior = new Character('warrior', 'Swordsman', 50, 1, 20, 10);
+
+console.log(warrior);
--- src/js/character.js
+++ src/js/character.js
@@ -1,13 +1,13 @@
 export default class Character {
   constructor(name, type, health, level, attack, defence) {
     if (typeof name !== 'string' || name.length < 2 || name.length > 10) {
-      throw new error('name shall be in string format and not less than 2 or more than 10 characters in length');
+      throw new Error('name shall be in string format and not less than 2 or more than 10 characters in length');
     } else {
       this.name = name;
     }
 
     if (Character.types.includes(type) === false) {
-      throw new error('type is not properly defined');
+      throw new Error('type is not properly defined');
     } else {
       this.type = type;
     }
